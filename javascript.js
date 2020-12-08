004fdbc315b562d1d39ecad0c68200c089ddaa6f
Kitiyaparnnn <parn.takhum@gmail.com>
2020-12-08 19:43:45 +0700

    v.4

--- javascript.js
+++ javascript.js
@@ -2,31 +2,50 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('form').onsubmit = function () {
         const task = document.querySelector('#task')
 
-        const list = document.createElement('p')
+        const list = document.createElement('tr')
         // list.innerHTML = document.querySelector("#new-task").value
-        const todo = document.createElement('span')
+        const subject = document.createElement('td')
         const sub = document.querySelector("#sub").value
+        const semester = document.createElement('td')
         const sem = document.querySelector("#sem").value
+        const grade = document.createElement('td')
         const gpa = document.querySelector("#gpa").value
+        const credit = document.createElement('td')
+        const cre = document.querySelector("#cre").value
         
-        todo.innerHTML = sub +' '+ sem +' '+ gpa
+        //todo.innerHTML = sem +'  '+ sub +'  '+ gpa
+        subject.innerHTML = sub
+        semester.innerHTML = sem
+        grade.innerHTML = gpa
+        credit.innerHTML = cre
         const deleteButton = document.createElement('button')
-        deleteButton.className = 'btn btn-danger m-2 h-'
+        deleteButton.className = 'btn btn-danger m-2 h-2'
         deleteButton.innerHTML = 'x'
         deleteButton.onclick = function () {
             task.removeChild(list)
         }
         if (sub != '' && sem != '' && gpa != '' ) {
-            list.append(todo)
+            list.append(semester)
+            list.append(subject)
+            list.append(grade)
+            list.append(credit)
             list.append(deleteButton)
             task.append(list)
+            //reset input
+            document.querySelector("#sub").value = ""
+            document.querySelector("#sem").value = ""
+            document.querySelector("#gpa").value = ""
+            document.querySelector("#cre").value = ""
         }
         else{
             alert('ใส่ข้อมูลด้วยจ้า')
         }
 
-        //reset input
-        //document.querySelector("#new-task").value = ""
+        //calculate gpa
+        const real_gpa = 
+
+
+
         return false
     }
 })
